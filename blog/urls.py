c70ac7b7d76352c30d0f546aa2f15ee84886671c
ShemmyYo <rabiliamemo@gmail.com>
2023-03-16 11:04:35 +0000

    linked Accounts and loging functionaliy as well as installed and copied allauth account folders

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import RecipeListView
+from .views import RecipeDetailView
 
 
 urlpatterns = [
-    path('', RecipeListView.as_view(), name='home')
+    path('', RecipeListView.as_view(), name='home'),
+    path('<slug:slug>/', RecipeDetailView.as_view(), name='recipe_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
 from django.views.generic import ListView
 from .models import Recipe
 
@@ -9,3 +12,24 @@ class RecipeListView(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_date')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class RecipeDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(active=True).order_by('created_date')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "post": recipe,
+                "comments": comments,
+                "liked": liked,
+
+            }
+        )
