135e8651fb8c098e14bd0054db1e2745f103b40a
ShemmyYo <rabiliamemo@gmail.com>
2023-04-11 19:32:54 +0000

    created and bootstrap'ed Edit Profile form

--- members/views.py
+++ members/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
+from .forms import SignUpForm, EditProfileForm
 
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
 
 
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('edit-profile')
 
