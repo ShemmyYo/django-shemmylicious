e9ecb650366da23991eb80394455aafd7d1ba3ce
ShemmyYo <rabiliamemo@gmail.com>
2023-04-12 07:17:18 +0000

    created login and logout views and urls

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    path('login/', views.login_user, name='login-user'),
+    path('logout/', views.logout_user, name='logout-user'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit-profile'),
+    path('password/', PasswordsChangeView.as_view(template_name='registration/password_change.html'), name='password'),
+    
 ]
--- members/views.py
+++ members/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib import messages
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None: 
+            login(request, user)
+            messages.success(request, (f"Successfully logged in. Welcome to your profile page {{ user }}"))
+            return redirect('my-profile')
+        else:
+            messages.success(request, ("There was an error logging in"))
+            return redirect('login')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Successfully logged out"))
+    return redirect('start')
+    
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('edit-profile')
 
 
 class UserRegisterView(generic.CreateView):
