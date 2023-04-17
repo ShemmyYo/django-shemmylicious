112aadfe163a8122c36c362c7e98e1d19242ca85
ShemmyYo <rabiliamemo@gmail.com>
2023-04-17 16:37:41 +0000

    clicking Category now brings you to view by category

--- members/urls.py
+++ members/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', views.login_user, name='login-user'),
     path('logout/', views.logout_user, name='logout-user'),
     path('register/', UserRegisterView.as_view(), name='register'),
-    path('<int:pk>/profile', ShowProfilePageView.as_view, name='show-profile'),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show-profile'),
     path('edit_profile/', UserEditView.as_view(), name='edit-profile'),
     path('password/', PasswordsChangeView.as_view(template_name='registration/password_change.html'), name='password'),
     
