3d24379eb19d7ae7150399b98dfe4929e1236ed4
ShemmyYo <rabiliamemo@gmail.com>
2023-04-17 19:04:37 +0000

    modifies style of the profile links

--- members/views.py
+++ members/views.py
@@ -15,7 +15,7 @@ class ShowProfilePageView(DetailView):
     template_name = 'profile/user_profile.html'
 
     def get_context_data(self, *args, **kwargs):
-        context = super(ShowProfilePageView, self).get_context_data(**kwargs)
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
         page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
         context["page_user"] = page_user
 
