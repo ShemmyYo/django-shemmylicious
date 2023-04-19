bb7d2ad18236e1a2e9d23806012cd5927fdea79a
ShemmyYo <rabiliamemo@gmail.com>
2023-04-19 07:48:57 +0000

    bootstraped add recipe page

--- blog/forms.py
+++ blog/forms.py
@@ -39,18 +39,18 @@ class AddRecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = [
-            'recipe_title',
+            # 'recipe_title',
             'category',
-            'featured_comment',
+            # 'featured_comment',
             'recipe_ingridients',
             'recipe_instructions',
-            'excerpt',
-            'featured_image',
+            # 'excerpt',
+            # 'featured_image',
         ]
 
         labels = {
             'recipe_title': '',
-            'category': '',
+            'category': ' * Choose Category',
             'featured_comment': 'Type in a short but catchy comment which will be featured with your recipe... Note: Shemmylicious Food will apper as default!',
             'recipe_ingridients': '',
             'recipe_instructions': '',
@@ -59,13 +59,13 @@ class AddRecipeForm(forms.ModelForm):
         }
         widgets = {
             'recipe_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type in Recipe Title'}),
-            'category': forms.Select(attrs={'class': 'form-select'}),
+            'category': forms.Select(attrs={'class': 'form-select-lg, mb-3'}),
             'featured_comment': forms.TextInput(attrs={'class': 'form-control'}),
-            'recipe_ingridients': SummernoteWidget(attrs={'summernote': {'width': '100%', 'height': '400px', 'placeholder': 'Type in ingridients here...'}}),
-            'recipe_instructions': SummernoteWidget(attrs={'summernote': {'width': '100%', 'height': '400px', 'placeholder': 'Provide instructions here...'}}),
+            'recipe_ingridients': SummernoteWidget(attrs={'summernote': {'width': '100%', 'height': '300px', 'placeholder': 'Type in ingridients here...'}}),
+            'recipe_instructions': SummernoteWidget(attrs={'summernote': {'width': '100%', 'height': '300px', 'placeholder': 'Provide instructions here...'}}),
             'excerpt': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Additional info info (e.g. nutrition facts etc.) as required go here...' }),
             'author': forms.Select(attrs={'class': 'form-select'}),
-            'featured_image': forms.FileInput(attrs={'class': "form-control", 'type': "file"}),
+            'featured_image': forms.FileInput(attrs={'class': "form-control, mb-3", 'type': "file"}),
         }
 
 
