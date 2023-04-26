bd798f138744415945780d3dd50d266e085d3dd9
ShemmyYo <rabiliamemo@gmail.com>
2023-04-26 22:46:19 +0000

    modifed js as console error

--- static/js/script.js
+++ static/js/script.js
@@ -2,12 +2,9 @@ gsap.registerPlugin(Flip);
 
 const links = document.querySelectorAll(".nav-item a");
 const activeNav = document.querySelector(".active-nav");
-
 const myCarouselElement = document.querySelector('#carouselExampleInterval');
 const carousel = new bootstrap.Carousel(myCarouselElement, {});
-
 const sectionBlank = document.querySelector('.section-blank');
-
 const indexHero = document.querySelector('.hero');
 const indexSlider = document.querySelector('.index-slider');
 const indexHeadline = document.querySelector('.headline');
@@ -16,8 +13,7 @@ const navbar = document.querySelector('.navbar');
 const footer = document.querySelector('.footer');
 const innerText = document.querySelector('.inner-text');
 
-
-
+// Index automations
 const tl = new TimelineMax();
 
 tl.fromTo(sectionBlank, 0.0001, { height: "100%" }, { height:"0%" })
@@ -32,10 +28,8 @@ tl.fromTo(sectionBlank, 0.0001, { height: "100%" }, { height:"0%" })
 .fromTo(myCarouselElement, 1.5, { opacity: 0, y: 60 }, { opacity: 1, y: 0 }, "-=2.5")
 .fromTo(innerText, 1.5, { opacity: 0, x: -60 }, { opacity: 1, x: 0 }, "-=2.0")
 
-
-
+// Underline Manu
 links.forEach(link => {
-    
     link.addEventListener('mouseover', () => {
         // move underline
         const state = Flip.getState(activeNav);
@@ -47,14 +41,4 @@ links.forEach(link => {
         })
     });
 })
-
 // Credits: https://www.youtube.com/watch?v=xB27AuRa5h4
-
-
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
-
