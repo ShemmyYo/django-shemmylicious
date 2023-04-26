7d995b130c1a3aa9713a765b76506041c896ee2f
ShemmyYo <rabiliamemo@gmail.com>
2023-04-26 21:23:07 +0000

    deployment with whitenoise disabled

--- shemmylicious/settings.py
+++ shemmylicious/settings.py
@@ -37,6 +37,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ["shemmylicious.herokuapp.com", "localhost"]
 
 
