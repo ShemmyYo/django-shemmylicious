da79efadb251ea93415060b31ab518a8d9bb80f3
ShemmyYo <rabiliamemo@gmail.com>
2023-05-01 13:29:11 +0000

    DEBUG = False

--- shemmylicious/settings.py
+++ shemmylicious/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG', False)
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
