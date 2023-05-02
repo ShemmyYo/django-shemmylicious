5180814f7de9fc9498e0df5b7dd62b8176cf2ded
ShemmyYo <rabiliamemo@gmail.com>
2023-05-02 19:46:34 +0000

    python linter check members urls.py, uploaded pics to readme folder

--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views 
+from django.contrib.auth import views as auth_views
 from .views import UserRegisterView, UserEditView, PasswordsChangeView
 from .views import EditProfilePageView, CreateProfilePageView
 from .views import ShowProfilePageView
@@ -9,11 +9,18 @@ from . import views
 urlpatterns = [
     path('login/', views.login_user, name='login-user'),
     path('logout/', views.logout_user, name='logout-user'),
-    path('register/', UserRegisterView.as_view(), name='register'),
-    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show-profile'),
-    path('<int:pk>/edit_profile/', EditProfilePageView.as_view(), name='edit-profile-page'),
-    path('create_profile/', CreateProfilePageView.as_view(), name='create-profile-page'),
+    path('register/',
+         UserRegisterView.as_view(), name='register'),
+    path('<int:pk>/profile/',
+         ShowProfilePageView.as_view(), name='show-profile'),
+    path('<int:pk>/edit_profile/',
+         EditProfilePageView.as_view(), name='edit-profile-page'),
+    path('create_profile/',
+         CreateProfilePageView.as_view(), name='create-profile-page'),
     path('edit_profile/', UserEditView.as_view(), name='edit-profile'),
-    path('password/', PasswordsChangeView.as_view(template_name='registration/password_change.html'), name='password'),
-    
+    path('password/',
+         PasswordsChangeView.as_view(
+            template_name='registration/password_change.html'
+            ),
+         name='password'),
 ]
